332b6e6f40230d951b9508791b8d3ea682829e9b
Nilay Brahmbhatt <brahmbhattnilay@gmail.com>
2024-06-27 20:49:26 +0530

    comments added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,28 @@
  * The page fetches an initial list of Pokémon from the PokéAPI and provides it to the
  * application through the PokemonProvider context. The page includes a search form
  * and a list of Pokémon that can be filtered based on the user's search input.
- * 
+ *
  * Components:
  * - PokemonList: A component that displays a list of Pokémon.
  * - SearchForm: A component that includes a search box for filtering the Pokémon list.
- * 
+ *
  * Contexts:
  * - PokemonProvider: A context provider that supplies the initial Pokémon data to the app.
- * 
+ *
  * Functions:
  * - fetchInitialPokemons: An asynchronous function that fetches the initial list of Pokémon
  *   from the PokéAPI with a limit of 40 Pokémon and assigns an ID to each Pokémon.
- * 
+ *
  * SEO:
  * - The initial data fetched is used to improve SEO by providing content to search engines
  *   during the initial render of the page.
- * 
+ *
  * @returns The Home component wrapped in the PokemonProvider with initial Pokémon data.
  */
 
 import PokemonList from "@/components/PokemonList";
 import SearchForm from "@/components/SearchBox";
 import { PokemonProvider } from "@/contexts/PokemonContext";
-
 
 async function fetchInitialPokemons() {
   const response = await fetch(
